aca74a197a88830fa1ccb68a3b0c0cca6fdec878
ReD_Sky <redxsky59@gmail.com>
2017-11-15 21:48:38 +0300

    Added DataBase Helper

---
+++ TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/DBHelper.java
@@ -0,0 +1,37 @@
+package zzzkvidi4.com.testandroidapplication1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Red Sky on 15.11.2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "localDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table game ("
+                + "id_game integer primary key autoincrement,"
+                + "name text not null,"
+                + "display_name text not null,"
+                + "description text not null,"
+                + "icon blob not null" + ");");
+        db.execSQL("create table game_match ("
+                + "id_game_match integer primary key autoincrement,"
+                + "id_game integer,"
+                + "display_name text not null,"
+                + "description text not null,"
+                + "icon blob not null,"
+                + " foreign key (id_game) references game (id_game));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
