a411a34ff68e15005a267bdfb8c32db1e4620442
ReD_Sky <redxsky59@gmail.com>
2017-12-11 00:22:41 +0300

    Fixed bags in GameFieldController.

--- TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/seqRepeater/GameFieldController.java
+++ TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/seqRepeater/GameFieldController.java
@@ -22,29 +22,33 @@ public class GameFieldController  implements GameController {
     private int fieldHeight;
     private boolean isTouchable = false;
     private Activity activity;
+    private int difficulty;
 
-    public GameFieldController(Activity activity, int fieldWidth, int fieldHeight){
+    public GameFieldController(Activity activity, int fieldWidth, int fieldHeight, int difficulty){
         gameField = new GameField(fieldWidth, fieldHeight);
         this.fieldHeight = fieldHeight;
         this.fieldWidth = fieldWidth;
         this.activity = activity;
+        this.difficulty = difficulty;
     }
 
     @Override
     public void initializeField(int canvasWidth, int canvasHeight){
         gameField.initialize(canvasWidth, canvasHeight);
-        int cardWidth = gameField.getElemWidth(canvasWidth);
-        int cardHeight = gameField.getElemHeight(canvasHeight);
     }
 
     @Override
     public void draw(Canvas canvas) {
+        Paint mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
+        int radius = gameField.getElemWidth(canvas.getWidth())/2;
         for (int i = 0; i < fieldHeight; ++i){
             for (int j = 0; j < fieldWidth; ++j){
                 SeqRepeaterGameObject elem = gameField.getField()[i][j];
-                Paint paint = new Paint();
-                paint.setColor(elem.getColor());
-                canvas.drawCircle(elem.getX(), elem.getY(), 10, paint);
+                mPaint.setColor(elem.getColor());
+                canvas.drawCircle(elem.getX()+radius, elem.getY()+radius, radius, mPaint);
+                canvas.save();
             }
         }
     }
@@ -54,20 +58,29 @@ public class GameFieldController  implements GameController {
         if (isTouchable) {
             gameField.checkOnTouch((int) event.getX(), (int) event.getY());
         }
-        if (gameField.gameFinished()){
+        if (gameField.gameFinished()) {
             Timer timer = new Timer();
-            timer.schedule(new ShowFinalScoreTimerTask(), 2000);
+            timer.schedule(new ShowFinalScoreTimerTask(), 500);
         }
     }
 
     @Override
     public void startGameCycle() {
-        gameField.showWin();
+        int delayShow = 500;
+        int delay = fieldHeight*fieldWidth*GameField.ONE_CARD_DELAY + delayShow;
         Timer timer = new Timer();
-        timer.schedule(new HideFieldTimerTask(), 2000);
+        timer.schedule(new ShowWinTimerTask(), delayShow);
+        timer.schedule(new StartGameTimerTask(), delay);
     }
 
-    private class HideFieldTimerTask extends TimerTask {
+    private class ShowWinTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            gameField.showWin();
+        }
+    }
+
+    private class StartGameTimerTask extends TimerTask {
         @Override
         public void run() {
             isTouchable = true;
@@ -78,6 +91,9 @@ public class GameFieldController  implements GameController {
         @Override
         public void run() {
             Intent intent = new Intent(activity, GameFinishedActivity.class);
+            intent.putExtra("score", gameField.getGuessedCount());
+            intent.putExtra("id", 2);
+            intent.putExtra("difficulty", difficulty);
             activity.finish();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             activity.startActivity(intent);
