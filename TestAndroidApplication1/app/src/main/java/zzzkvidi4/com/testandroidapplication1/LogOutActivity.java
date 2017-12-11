f0718101ffd2d93a397162f2f1bf577705e17a59
Zzzkvidi4 <zzzkvidi4@gmail.com>
2017-12-12 00:28:03 +0300

    Created logic to log out user or turn back to menu activity

--- TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/LogOutActivity.java
+++ TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/LogOutActivity.java
@@ -1,8 +1,12 @@
 package zzzkvidi4.com.testandroidapplication1;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import com.vk.sdk.VKSdk;
 
 /**
  * Created by Red Sky on 11.12.2017.
@@ -15,8 +19,26 @@ public class LogOutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
         Button logout = (Button)findViewById(R.id.YesLogoutBtn);
-        //logout.setOnClickListener(new LogOutOnClickListener(this, false));
+        logout.setOnClickListener(new LogOutOnClickListener());
         Button cancel = (Button)findViewById(R.id.NoLogoutBtn);
-        //cancel.setOnClickListener(new BackOnClickListener(this));
+        cancel.setOnClickListener(new BackOnClickListener());
+    }
+
+    private class LogOutOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            VKSdk.logout();
+            SharedPreferences preferences = getSharedPreferences("user_info", MODE_PRIVATE);
+            preferences.edit().clear().apply();
+            finish();
+        }
+    }
+
+    private class BackOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
     }
 }
