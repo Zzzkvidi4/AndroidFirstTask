89c8c67e75accfca61406a4ee4f261aeedebfdc8
ReD_Sky <redxsky59@gmail.com>
2017-12-11 23:12:10 +0300

    Added java class for logout activity

---
+++ TestAndroidApplication1/app/src/main/java/zzzkvidi4/com/testandroidapplication1/LogOutActivity.java
@@ -0,0 +1,22 @@
+package zzzkvidi4.com.testandroidapplication1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Red Sky on 11.12.2017.
+ */
+
+public class LogOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+        Button logout = (Button)findViewById(R.id.backToMenuBtn);
+        //logout.setOnClickListener(new LogOutOnClickListener(this, false));
+        Button cancel = (Button)findViewById(R.id.backToGameBtn);
+        //cancel.setOnClickListener(new BackOnClickListener(this));
+    }
+}
